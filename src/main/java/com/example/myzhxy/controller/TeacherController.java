9a2d4501e3ccd45e5f6e651d8f5557f01b8401f2
hongxiaobin <2812181610@qq.com>
2022-10-13 11:45:05 +0800

    2812181610@qq.com

--- src/main/java/com/example/myzhxy/controller/TeacherController.java
+++ src/main/java/com/example/myzhxy/controller/TeacherController.java
@@ -13,7 +13,6 @@ import com.example.myzhxy.utils.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
