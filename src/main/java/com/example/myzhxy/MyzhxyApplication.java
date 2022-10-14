41304b6fbe77b5324800f719a63f7475552d9201
hongxiaobin <2812181610@qq.com>
2022-10-14 09:39:33 +0800

    2812181610@qq.com

--- src/main/java/com/example/myzhxy/MyzhxyApplication.java
+++ src/main/java/com/example/myzhxy/MyzhxyApplication.java
@@ -3,6 +3,9 @@ package com.example.myzhxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author hongxiaobin
+ */
 @SpringBootApplication
 public class MyzhxyApplication {
 
